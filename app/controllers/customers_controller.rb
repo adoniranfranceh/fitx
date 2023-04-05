73aa169fc80b1e2f08c330c6efe2212c199eded5
adoniran <adoniranfranca@gmail.com>
2023-04-05 13:35:14 -0300

    adicionando listagem e form para clientes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,31 @@ class CustomersController < ApplicationController
   def index
     @customers = Customer.all
   end
+
+  def show; end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create 
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to customers_path
+    else
+      render :new  
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, 
+                                     :document, 
+                                     :phone, 
+                                     :payment_at, 
+                                     :age, 
+                                     :weigth, 
+                                     :height)
+  end
 end
